04c6be69c748b0113a18e27bb8878d2db4675004
yurivilela <yurimarquez1704@gmail.com>
2021-08-24 12:21:10 -0300

    colocando sistema de busca e conectando-o com ordenacao

--- index.php
+++ index.php
@@ -10,15 +10,52 @@
         <?php
             require_once "includes/banco.php";
             require_once "includes/funcoes.php";
+
+            $ordem = $_GET['ordenar'] ?? "nome";
+            $chave = $_GET['busca'] ?? "";
         ?>
         <div id="body">
             <?php include_once "topo.php"; ?>
             <h1 class='index-titulo'>Escolha seu filme</h1>
+            <form method="get" id="busca" action="index.php">
+                Ordenar por:
+                <a href="index.php?ordenar=nome&busca=<?php echo $chave; ?>"> Nome</a> |
+                <a href="index.php?ordenar=produtora&busca=<?php echo $chave; ?>"> Produtora</a> |
+                <a href="index.php?ordenar=notaMaior&busca=<?php echo $chave; ?>"> Nota Alta</a> |
+                <a href="index.php?ordenar=notaMenor&busca=<?php echo $chave; ?>"> Nota Baixa</a> |
+                <a href="index.php"> Mostrar Todos</a> |
+                Buscar: <input type="text" name="busca" size="10" maxlength="40">
+                <input type="submit" value="OK">
+            </form>
             <table class="listagem">
                 <?php
                 $joinQuery = "SELECT filmes.nome, filmes.cod, filmes.capa, generos.genero, produtoras.produtora FROM filmes 
                               JOIN generos ON filmes.genero = generos.cod
-                              JOIN produtoras ON filmes.produtora = produtoras.cod";
+                              JOIN produtoras ON filmes.produtora = produtoras.cod ";
+
+                if (!empty($chave))
+                {
+                    $joinQuery .= "WHERE filmes.nome like '%$chave%'
+                    OR produtoras.produtora like '%$chave%'
+                    OR generos.genero like '%$chave%' ";
+                }
+
+                switch ($ordem)
+                {
+                    case "produtora":
+                        $joinQuery .= "ORDER BY produtoras.produtora";
+                        break;
+                    case "notaMaior":
+                        $joinQuery .= "ORDER BY filmes.nota DESC";
+                        break;
+                    case "notaMenor":
+                        $joinQuery .= "ORDER BY filmes.nota ASC";
+                        break;
+                    default:
+                        $joinQuery .= "ORDER BY filmes.nome";
+                        break;
+                }
+
                 $codigo = $_GET['cod'] ?? 0;
                 $busca = $banco->query("$joinQuery");
 
--- rodape.php
+++ rodape.php
@@ -1,6 +1,6 @@
 <?php
 echo "<footer>";
 echo "<p>Acessador por " . $_SERVER['REMOTE_ADDR'] . " em " . date('d/m/Y') .".</p>";
-echo "<p> Desensolvivo por Yuri Vilela estudando pela estudonauta &copy;</p>";
+echo "<p> Desensolvivo por Yuri Vilela estudando pela estudonauta &copy;!</p>";
 echo "</footer>";
 $banco->close();
